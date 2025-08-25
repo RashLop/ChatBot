04611a421b4678e5fb3d5baa0830c7e451e1d594
RashLop <lopezmejiarasshid@outlook.com>
2025-08-25 14:37:11 -0400

    feat: Saludar por genero añadido

--- src/greeting.js
+++ src/greeting.js
@@ -1,7 +1,17 @@
+export function getGreeting(nombre, genero, date = new Date()) {
+        const h = date.getHours(); // 0–23
+        let saludoHora = "";
 
-export function getGreeting(date = new Date()) {
-    const h = date.getHours(); // 
-        if (h >= 6 && h < 12) return "Buenos días";
-        if (h >= 12 && h < 19) return "Buenas tardes";
-    return "Buenas noches";
+    if (h >= 6 && h < 12) saludoHora = "Buenos días";
+    else if (h >= 12 && h < 19) saludoHora = "Buenas tardes";
+    else saludoHora = "Buenas noches";
+
+    let trato = "";
+    if (genero === "M" || genero === "m") {
+        trato = "Señor";
+    } else if (genero === "F" || genero === "f") {
+        trato = "Señorita";
+    } 
+
+    return `${saludoHora}, ${trato} "${nombre}"`;
 }
--- src/presenter.js
+++ src/presenter.js
@@ -1,16 +1,20 @@
+
 import { getGreeting } from './greeting.js';
-const first = document.querySelector("#nombre");
+
+const inputNombre = document.querySelector("#nombre");
+const inputGenero = document.querySelector("#genero");
 const formSaludoNom = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
 formSaludoNom.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const nombre = (first.value || "").trim();
-  if (nombre) {
-    const saludo = getGreeting();
-    div.textContent = `${saludo} "${nombre}"`;
+  const nombre = (inputNombre.value || "").trim();
+  const genero = (inputGenero.value || "").trim();
+
+  if (nombre && genero) {
+    div.textContent = getGreeting(nombre, genero);
   } else {
-    div.textContent = "Por favor ingresa tu nombre";
+    div.textContent = "Por favor ingresa tu nombre y género ";
   }
 });
